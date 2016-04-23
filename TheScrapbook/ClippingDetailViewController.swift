3761e53fc3d41e3213548c6122f604026ab729f7
Zhe Xian Lee <zxlee618@gmail.com>
2016-04-23 16:34:02 +1000

    Implements tap on the image in ClippingDetailsViewController to make it fullscreen and zoom-able

--- TheScrapbook/ClippingDetailViewController.swift
+++ TheScrapbook/ClippingDetailViewController.swift
@@ -18,13 +18,18 @@ class ClippingDetailViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var noteTextView: UITextView!
     @IBOutlet weak var imageViewAspectConstraint: NSLayoutConstraint!
     @IBOutlet weak var imageViewEqualHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var noteTextViewZeroHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTopContraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewEqualWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var dateCreatedLabelZeroHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setToolbarHidden(true, animated: true)
         
         imageView.image = image
-
+       
     
         imageView.userInteractionEnabled = true
         let tap = UITapGestureRecognizer(target: self, action: "didTapImageView:")
@@ -35,6 +40,7 @@ class ClippingDetailViewController: UIViewController, UIScrollViewDelegate {
     var imageViewFullscreen = false
     
     func didTapImageView(gestureRecognizer: UITapGestureRecognizer) {
+        noteTextView.resignFirstResponder()
         imageViewFullscreen = !imageViewFullscreen
         self.navigationController?.setNavigationBarHidden(imageViewFullscreen, animated: true)
         
@@ -42,9 +48,17 @@ class ClippingDetailViewController: UIViewController, UIScrollViewDelegate {
             self.scrollView.minimumZoomScale = 1
             self.scrollView.maximumZoomScale = 7
             
+            self.imageViewEqualHeightConstraint.priority = 750
+            self.imageViewAspectConstraint.priority = 250
+            self.noteTextViewZeroHeightConstraint.priority = 999
+            self.imageViewEqualWidthConstraint.constant = 0
+            self.imageViewTopContraint.constant = 0
+            self.dateCreatedLabelZeroHeightConstraint.priority = 999
+            
             UIView.animateWithDuration(0.5, animations: { () -> Void in
-                self.imageViewEqualHeightConstraint.priority = 750
-                self.imageViewAspectConstraint.priority = 250
+
+                self.view.layoutIfNeeded()
+                self.view.backgroundColor = UIColor.blackColor()
             })
             
         }
@@ -53,9 +67,17 @@ class ClippingDetailViewController: UIViewController, UIScrollViewDelegate {
             self.scrollView.minimumZoomScale = 1
             self.scrollView.maximumZoomScale = 1
         
+            self.imageViewEqualHeightConstraint.priority = 250
+            self.imageViewAspectConstraint.priority = 750
+            self.noteTextViewZeroHeightConstraint.priority = 1
+            self.imageViewEqualWidthConstraint.constant = -20
+            self.imageViewTopContraint.constant = 8
+            self.dateCreatedLabelZeroHeightConstraint.priority = 1
+            
             UIView.animateWithDuration(0.5, animations: { () -> Void in
-                self.imageViewEqualHeightConstraint.priority = 250
-                self.imageViewAspectConstraint.priority = 750
+
+                self.view.layoutIfNeeded()
+                self.view.backgroundColor = UIColor.whiteColor()
             })
         }
     }
@@ -69,10 +91,7 @@ class ClippingDetailViewController: UIViewController, UIScrollViewDelegate {
     }
 
     
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
-    }
+
     
     
     
